9f5c43d3eebc1f4f14746c4465fa9ce6e879ba5a
Juanco <juanffo874@gmail.com>
2025-02-13 00:45:39 -0600

    Define los primeros estilos del reloj

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
+import Work from './components/Work';
 
 function App() {
 
   return (
     <>
-      <div className='text-center'>Hola mundo</div>
+      <div>
+        <h2 className="mx-auto mt-24 mb-10 font-bold text-4xl text-gray-300 text-center">
+          Reloj pomodoro
+        </h2>
+        <div className="grid grid-cols-2 mx-10">
+          <div className='flex justify-center items-center'>
+            <Work />
+          </div>
+          <div className='flex justify-center items-center'>
+            <h1 className="text-gray-300">Hola</h1>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Work.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Work = () => {
+
+    //Definendo los ciclos
+    const WSECOND = 25 * 60
+    const WMINUTE = 60 * 60
+    const WHOUR = 60 / 4
+    const CICLE = 4
+
+  return (
+    <>
+        <div className='my-20 max-w-lg mx-auto border bg-gray-800 p-10 rounded-lg'>
+            <h1 className='text-2xl font-bold text-gray-300'>Tiempo</h1>
+            {/* <p className='text-gray-400'>Estás en modo trabajo.</p> */}
+            
+        </div>
+    </>
+  )
+}
+
+export default Work
