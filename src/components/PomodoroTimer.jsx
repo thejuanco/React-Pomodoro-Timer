825e0112efd64f3f2850e655defc74264b2a69b6
Juanco <juanffo874@gmail.com>
2025-02-21 19:37:07 -0600

    Integra los cambios para los tiempos de trabajo y descanso

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 const PomodoroTimer = () => {
 
-  const WORK_TIME = 25 * 60;
+  const WORK_TIME = 2 * 2;
   const BREAK_TIME = 5 * 60;
   const LONG_TIME = 15 * 60;
   const TOTAL_CYCLES = 4;
@@ -19,21 +19,44 @@ const PomodoroTimer = () => {
   }
 
   useEffect(() => {
-    if(!isActive) return;
-    const interval =setInterval(() => {
-      setTime((prevTime) => (prevTime > 0 ? prevTime -1 : 0))
-    }, 1000)
-    return () => clearInterval(interval)
-  }, [isActive])
+    let interval = null;
+
+    if (isActive && time > 0) {
+      interval = setInterval(() => {
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+      }, 1000);
+    } else if(time === 0){
+      if(isWork){
+        if(cycle < TOTAL_CYCLES){
+          setTime(BREAK_TIME);
+          setCycle((newCycle) => newCycle + 1);
+        } else if(cycle == TOTAL_CYCLES){
+          setTime(LONG_TIME)
+          setCycle(1)
+        }
+      } else {
+        setTime(WORK_TIME)
+      }
+      setIsWork(prevIsWork => !prevIsWork)
+    }
+
+    return () => interval ? clearInterval(interval) : null
+  }, [isActive, time, isWork, cycle])
 
   const toggleTimer = () => {
     setIsActive(!isActive)
   }
 
+  const resetTimer = () => {
+    setIsActive(false)
+    setTime(WORK_TIME)
+    setIsWork(true)
+  }
+
   return (
     <>
-      <div className="w-full md:w-1/3 bg-gray-800 text-white rounded-md p-2">
-        <h1 className="text-center text-xl font-bold text-blue-400">Trabajo</h1>
+      <div className="w-full md:w-1/3 bg-gray-800 text-white rounded-lg p-3">
+        <h1 className="text-center text-xl font-bold mt-2 text-blue-400">{isWork ? "Trabajo" : "Descanso"}</h1>
         <div className="flex flex-col items-center space-y-4 p-2">
           <span className="font-bold text-6xl mt-8">{formatTime(time)}</span>
           <p className="text-gray-300">Ciclo: {cycle} / {TOTAL_CYCLES}</p>
@@ -44,9 +67,11 @@ const PomodoroTimer = () => {
                 toggleTimer()
               }}
             >
-              Iniciar
+              {isActive ? "Pausar" : "Iniciar"}
             </button>
-            <button className="bg-white text-gray-900 py-2 px-3 rounded-lg font-semibold hover:bg-gray-200">
+            <button className="bg-white text-gray-900 py-2 px-3 rounded-lg font-semibold hover:bg-gray-200"
+              onClick={() => resetTimer()}
+            >
               reiniciar
             </button>
           </div>
