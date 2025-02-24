da2c49ae8a5c479f13a48b6595b768c6cd9f9923
Juanco <juanffo874@gmail.com>
2025-02-24 14:14:19 -0600

    Crea un nav para poder navegar mejor dentro de la app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import TaskList from '../components/TaskList'
 import PomodoroTimer from '../components/PomodoroTimer'
+import NavHome from '../components/NavHome';
 
 const Home = () => {
   return (
     <>
+      <NavHome />
       <div>
-        <h2 className="mx-auto mt-24 mb-10 font-bold text-4xl text-gray-300 text-center">
+        <h2 className="mx-auto mt-28 mb-10 font-bold text-4xl text-gray-300 text-center">
           Reloj pomodoro
         </h2>
         <div className="flex justify-center items-center mx-20 gap-8">
